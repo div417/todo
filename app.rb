20a11163b2087fdafdb1666cbbdc5d4263844581
Divyansh Sharma <divyansh9595@gmail.com>
2017-06-14 20:32:40 +0530

    basic structure

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'sinatra'
+class Todo 
+    attr_accessor :task, :done, :urgent, :important
+    def initialize task, urgent, important, done
+        @task=task
+        @done=done
+        @urgent=urgent
+        @important=important
+    end
+end
+Tasks=[]
+get '/' do
+    data=Hash.new
+    data[:tasks]=Tasks
+    erb :main, locals: data
+end
+post '/add' do
+       task=params["task"]
+       puts task
+       if task != nil
+       if params.has_key?("urgent")==true
+           urgent=true
+       else
+           urgent=false
+       end
+       if params.has_key?("important")==true
+           important=true
+       else
+          important=false
+       end
+       todo=Todo.new task,urgent,important,false
+       Tasks << todo
+       end
+       return redirect '/'
+end
